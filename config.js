ef76fe0af622b1b843f5502a7baaa32858631b57
Thomas Chun <tchun2055@gmail.com>
2019-05-14 12:19:29 -0700

    connected to mongodb atlas with user auth

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/scr',
+  DATABASE_URL: process.env.DATABASE_URL || process.env.MONGODB_URI,
   TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/scr-test',
+    process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/card-flipping-test',
   MONGODB_OPTIONS: {
     useNewUrlParser: true,
   },
